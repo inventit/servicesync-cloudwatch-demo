f1697fd69346092c12abd2d7c54f0926d8083780
myamaguchi806 <myamaguchi@yourinventit.com>
2015-06-16 02:50:20 +0900

    [modify] sscw.c指摘事項修正２, snprinfの例外処理を追加

--- c/src/sscw.c
+++ c/src/sscw.c
@@ -20,8 +20,9 @@
 #include <string.h>
 #include <servicesync/moat.h>
 
-#define TAG "Sample"
-
+#define TAG "SSCW"
+#define UPLOAD_INTERVAL (60) /* sec */
+#define MODEL_NAME "SensingData"
 
 typedef struct {
     sse_float temperature;
@@ -41,52 +42,64 @@ get_sensor_data(SensingData* out_sensor_data)
 }
 
 static void
-upload_cpu_usage_result_proc(Moat in_moat, sse_char *in_urn, sse_char *in_model_name, sse_int in_request_id, sse_int in_result, sse_pointer in_user_data)
+upload_sensor_data_result_proc(Moat in_moat, sse_char *in_urn, sse_char *in_model_name, sse_int in_request_id, sse_int in_result, sse_pointer in_user_data)
 {
     if (in_result == SSE_E_OK) {
-        SSE_LOG_INFO(TAG, "moat_send_notificaion() has been complated.");
+        MOAT_LOG_INFO(TAG, "moat_send_notificaion() has been complated.");
     } else {
-        SSE_LOG_INFO(TAG, "moat_send_notificaion() has failed with [%d].", in_result);
+        MOAT_LOG_INFO(TAG, "moat_send_notificaion() has failed with [%d].", in_result);
     }
 }
 
 
 static sse_bool
-upload_cpu_usage(sse_int in_timer_id, sse_pointer in_user_data)
+
+upload_sensor_data(sse_int in_timer_id, sse_pointer in_user_data)
 {
     sse_int err;
+    sse_int urn_err;
     sse_int request_id;
     MoatObject* object = NULL;
-    SensingData cpu_usage;
+    SensingData sensor_data;
     UserContext* ctx = (UserContext*)in_user_data;
     sse_char urn[512];
     
-    err = get_sensor_data(&cpu_usage);
+    err = get_sensor_data(&sensor_data);
     
     if (err != SSE_E_OK) {
-        SSE_LOG_ERROR(TAG, "get_cpu_usage() has failed with [%d].", err);
+        MOAT_LOG_ERROR(TAG, "get_sensor_data() has failed with [%d].", err);
         return sse_true;
     }
     
     object = moat_object_new();
     if (object == NULL) {
-        SSE_LOG_ERROR(TAG, "moat_object_new() has failed.");
+        MOAT_LOG_ERROR(TAG, "moat_object_new() has failed.");
         return sse_true;
     }
-    moat_object_add_float_value(object, "temperature", cpu_usage.temperature, sse_false);
-    moat_object_add_float_value(object, "humidity", cpu_usage.humidity, sse_false);
+    moat_object_add_float_value(object, "temperature", sensor_data.temperature, sse_false);
+    moat_object_add_float_value(object, "humidity", sensor_data.humidity, sse_false);
     moat_object_add_int64_value(object, "timestamp", moat_get_timestamp_msec(), sse_false);
     
-    snprintf(urn, sizeof(urn) - 1, "urn:moat:%s:upload-cpu-usage:1.0.0", moat_get_package_urn(ctx->moat));
+    urn_err = snprintf(urn, sizeof(urn) - 1, "urn:moat:%s:upload-cpu-usage:1.0.0", moat_get_package_urn(ctx->moat));
+    if(urn_err < 0)
+    {
+        MOAT_LOG_ERROR(TAG, "Failed to get package urn from moat object.");
+        return sse_true;
+    }
+    else if(urn_err != sizeof(urn))
+    {
+        MOAT_LOG_WARN(TAG, "urn info might be lacked. %d, %d",urn_err,sizeof(urn));
+    }
+    
     request_id = moat_send_notification(ctx->moat,                    /* Moat Instance */
                                         urn,                          /* URN */
                                         NULL,                         /* Key */
-                                        "SensingData",                   /* Model name */
+                                        MODEL_NAME,                   /* Model name */
                                         object,                       /* Data collection */
-                                        upload_cpu_usage_result_proc, /* Callback */
+                                        upload_sensor_data_result_proc, /* Callback */
                                         ctx);                         /* User data */
     if (request_id < 0) {
-        SSE_LOG_ERROR(TAG, "moat_send_notification() has failed with [%d].", request_id);
+        MOAT_LOG_ERROR(TAG, "moat_send_notification() has failed with [%d].", request_id);
     }
     
     moat_object_free(object);
@@ -115,7 +128,7 @@ moat_app_main(sse_int in_argc, sse_char *argv[])
     /* register models */
     sse_memset(&model_mapper, 0, sizeof(model_mapper));
     err = moat_register_model(moat,          /* MOAT instance */
-                              "SensingData",    /* Model name */
+                              MODEL_NAME,    /* Model name */
                               &model_mapper, /* ModelMapper instance */
                               ctx);          /* Context */
     if (err != SSE_E_OK) {
@@ -123,14 +136,14 @@ moat_app_main(sse_int in_argc, sse_char *argv[])
     }
     
     /* set timer for monitoring cpu usage */
-    //get_cpu_usage(&(ctx->last), NULL); /* initialize last cpu count */
+    //get_sensor_data(&(ctx->last), NULL); /* initialize last cpu count */
     timer = moat_timer_new();
     if (timer == NULL) {
         goto error_exit;
     }
     timer_id = moat_timer_set(timer,            /* Timer instance */
                               3,                /* Interval sec   */
-                              upload_cpu_usage, /* Callback       */
+                              upload_sensor_data, /* Callback       */
                               ctx);             /* User data      */
     if (timer_id < 1) {
         goto error_exit;
@@ -139,12 +152,12 @@ moat_app_main(sse_int in_argc, sse_char *argv[])
     /* main loop */
     err = moat_run(moat);
     if (err != SSE_E_OK) {
-        SSE_LOG_ERROR(TAG, "moat_run() has failed with [%d].", err);
+        MOAT_LOG_ERROR(TAG, "moat_run() has failed with [%d].", err);
     }
     
     /* Teardown */
     moat_timer_free(timer);
-    moat_remove_model(moat, "SensingData");
+    moat_remove_model(moat, MODEL_NAME);
     sse_free(ctx);
     moat_destroy(moat);
     
